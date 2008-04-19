b3d0875388da7dd67bd3dd74d2ea9b065f474cff
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-04-19 19:13:45 +0000

    Tagging checker-0006.

---
+++ cfe/tags/checker/checker-0006/test/CodeGen/string-literal.c
@@ -0,0 +1,5 @@
+// RUN: clang -emit-llvm %s
+
+int main() {
+  char a[10] = "abc";
+}
---
+++ cfe/tags/checker/checker-0006/test/Rewriter/rewrite-try-catch.m
@@ -0,0 +1,23 @@
+// RUN: clang -rewrite-objc %s -o=-
+
+@interface Foo @end
+@interface GARF @end
+
+void foo() {
+  @try  { TRY(); } 
+  @catch (...) { SPLATCH(); @throw; }
+}
+
+int main()
+{
+
+@try  {
+   MYTRY();
+}
+
+@catch (Foo* localException) {
+   MYCATCH();
+   @throw;
+}
+}
+
