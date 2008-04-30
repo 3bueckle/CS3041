f14cfea45592be14708209c7e710ec7d0d8b7b3e
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-04-30 23:01:44 +0000

    Tagging checker-17.

---
+++ cfe/tags/checker/checker-17/test/Rewriter/objc-super-test.m
@@ -0,0 +1,15 @@
+// RUN: clang -rewrite-objc %s -o=-
+
+@interface SUPER
+- (int) MainMethod;
+@end
+
+@interface MyDerived : SUPER
+- (int) instanceMethod;
+@end
+
+@implementation MyDerived 
+- (int) instanceMethod {
+  return [super MainMethod];
+}
+@end
