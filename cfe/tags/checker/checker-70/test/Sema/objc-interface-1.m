38712d0f3b1fe74c9ebc2e7c98e7b5633f4c85bb
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-07-25 20:46:02 +0000

    Tagging checker-70.

---
+++ cfe/tags/checker/checker-70/test/Sema/objc-interface-1.m
@@ -0,0 +1,17 @@
+// RUN: clang %s -fsyntax-only -verify
+// rdar://5957506
+
+@interface NSWhatever :
+NSObject     // expected-error {{cannot find interface declaration for 'NSObject'}}
+<NSCopying>  // expected-error {{cannot find protocol definition for 'NSCopying'}}
+@end
+
+
+// rdar://6095245
+@interface A
+{
+  int x
+}  // expected-error {{expected ';' at end of declaration list}}
+@end
+
+
