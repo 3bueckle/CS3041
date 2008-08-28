f80e46c54580995c19ee9c9410a7bc881cedfd2c
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-08-28 21:29:10 +0000

    Tagging checker-85.

---
+++ cfe/tags/checker/checker-85/include/clang/Analysis/PathSensitive/ConstraintManager.h
@@ -0,0 +1,21 @@
+#ifndef CONSTRAINT_MANAGER_H
+#define CONSTRAINT_MANAGER_H
+
+namespace clang {
+
+class GRState;
+class GRStateManager;
+class RVal;
+
+class ConstraintManager {
+public:
+  virtual ~ConstraintManager();
+  virtual const GRState* Assume(const GRState* St, RVal Cond, bool Assumption,
+                                bool& isFeasible) = 0;
+};
+
+ConstraintManager* CreateBasicConstraintManager(GRStateManager& statemgr);
+
+} // end clang namespace
+
+#endif
