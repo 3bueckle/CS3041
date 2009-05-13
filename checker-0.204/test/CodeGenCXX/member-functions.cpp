0b57838a94dfb056684c2b31bf55a4c922cac18d
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-05-13 19:29:01 +0000

    Tagging checker-0.204.

---
+++ checker-0.204/test/CodeGenCXX/member-functions.cpp
@@ -0,0 +1,45 @@
+// RUN: clang-cc -emit-llvm %s -o %t &&
+struct C {
+  void f();
+  void g(int, ...);
+};
+
+// RUN: grep "define void @_ZN1C1fEv" %t | count 1 &&
+void C::f() {
+}
+
+void test1() {
+  C c;
+  
+// RUN: grep "call void @_ZN1C1fEv" %t | count 1 &&
+  c.f();
+  
+// RUN: grep "call void (.struct.C\*, i32, ...)\* @_ZN1C1gEiz" %t | count 1 &&
+  c.g(1, 2, 3);
+}
+
+
+struct S {
+  // RUN: grep "define linkonce_odr void @_ZN1SC1Ev" %t &&
+  inline S() { }
+  // RUN: grep "define linkonce_odr void @_ZN1SC1Ev" %t &&
+  inline ~S() { }
+  
+  
+  void f_inline1() { }
+  // RUN: grep "define linkonce_odr void @_ZN1S9f_inline2Ev" %t &&
+  inline void f_inline2() { }
+  
+  // RUN: grep "define internal void @_ZN1S1gEv" %t
+  static void g() { }
+};
+
+void test2() {
+  S s;
+  
+  s.f_inline1();
+  s.f_inline2();
+  
+  S::g();
+  
+}
