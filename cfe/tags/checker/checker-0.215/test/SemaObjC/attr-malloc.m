6055456114965f055ba2a1c619f0fe69a932d4b7
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-08-14 21:41:13 +0000

    Tagging checker-0.215.

---
+++ cfe/tags/checker/checker-0.215/test/SemaObjC/attr-malloc.m
@@ -0,0 +1,12 @@
+// RUN: clang-cc -verify -fsyntax-only -fblocks %s
+
+@interface TestAttrMallocOnMethods {}
+- (id) test1 __attribute((malloc)); // expected-warning{{'malloc' attribute only applies to function types}}
+- (int) test2 __attribute((malloc)); // expected-warning{{'malloc' attribute only applies to function types}}
+@end
+
+id bar(void) __attribute((malloc)); // no-warning
+
+typedef void (^bptr)(void);
+bptr baz(void) __attribute((malloc)); // no-warning
+
