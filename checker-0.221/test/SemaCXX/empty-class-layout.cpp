ad59df11ab5e574acf50cce85c54078913bd01ac
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-09-25 00:22:44 +0000

    Tagging checker-0.221.

---
+++ checker-0.221/test/SemaCXX/empty-class-layout.cpp
@@ -0,0 +1,26 @@
+// RUN: clang-cc -triple x86_64-unknown-unknown %s -fsyntax-only -verify 
+
+#define SA(n, p) int a##n[(p) ? 1 : -1]
+
+struct A { int a; };
+SA(0, sizeof(A) == 4);
+
+struct B { };
+SA(1, sizeof(B) == 1);
+
+struct C : A, B { };
+SA(2, sizeof(C) == 4);
+
+struct D { };
+struct E : D { };
+struct F : E { };
+
+struct G : E, F { };
+SA(3, sizeof(G) == 2);
+
+struct H { H(); };
+
+struct I : H { 
+  H h;
+};
+SA(4, sizeof(I) == 2);
