e9d60912c60efc3faac42111b8a9db3f4259eb56
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-10-22 06:12:21 +0000

    Removing cremebrulee-16.

--- cremebrulee-16/test/CodeGenCXX/address-of-fntemplate.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: clang-cc %s -emit-llvm -o - | FileCheck %s
-template <typename T> void f(T) {}
-
-void test() {
-  // CHECK: @_Z1fIiEvT_
-  void (*p)(int) = &f;
-}
-// CHECK: define linkonce_odr void @_Z1fIiEvT_
