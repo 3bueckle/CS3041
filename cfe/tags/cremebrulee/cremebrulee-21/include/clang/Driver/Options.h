8a33a30a835d2367ae4de9a9b997c054b75b84fa
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-11-19 00:20:22 +0000

    Tagging cremebrulee-21.

---
+++ cfe/tags/cremebrulee/cremebrulee-21/include/clang/Driver/Options.h
@@ -0,0 +1,34 @@
+//===--- Options.h - Option info & table ------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CLANG_DRIVER_OPTIONS_H
+#define CLANG_DRIVER_OPTIONS_H
+
+namespace clang {
+namespace driver {
+  class OptTable;
+
+namespace options {
+  enum ID {
+    OPT_INVALID = 0, // This is not an option ID.
+    OPT_INPUT,       // Reserved ID for input option.
+    OPT_UNKNOWN,     // Reserved ID for unknown option.
+#define OPTION(NAME, ID, KIND, GROUP, ALIAS, FLAGS, PARAM, \
+               HELPTEXT, METAVAR) OPT_##ID,
+#include "clang/Driver/Options.def"
+    LastOption
+#undef OPTION
+  };
+}
+
+  OptTable *createDriverOptTable();
+}
+}
+
+#endif
