ece1dc27f67a51c2e00f336b0491ed60fabaa3da
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2011-04-13 21:52:29 +0000

    Tagging checker-256.

---
+++ checker-256/test/SemaCXX/writable-strings-deprecated.cpp
@@ -0,0 +1,13 @@
+// RUN: %clang_cc1 -fsyntax-only -Wno-deprecated-writable-strings -verify %s
+// RUN: %clang_cc1 -fsyntax-only -fwritable-strings -verify %s
+// rdar://8827606
+
+char *fun(void)
+{
+   return "foo";
+}
+
+void test(bool b)
+{
+  ++b; // expected-warning {{incrementing expression of type bool is deprecated}}
+}
