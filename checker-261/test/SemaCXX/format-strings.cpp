1304a1098f25ece561e95505d2b648a000e3205c
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2012-02-02 05:39:54 +0000

    Tagging checker-261.

---
+++ checker-261/test/SemaCXX/format-strings.cpp
@@ -0,0 +1,19 @@
+// RUN: %clang_cc1 -fsyntax-only -verify -pedantic %s
+
+extern "C" {
+extern int scanf(const char *restrict, ...);
+extern int printf(const char *restrict, ...);
+}
+
+void f(char **sp, float *fp) {
+  // TODO: Warn that the 'a' length modifier is an extension.
+  scanf("%as", sp);
+
+  // TODO: Warn that the 'a' conversion specifier is a C++11 feature.
+  printf("%a", 1.0);
+  scanf("%afoobar", fp);
+}
+
+void g() {
+  printf("%ls", "foo"); // expected-warning{{format specifies type 'wchar_t *' but the argument has type 'const char *'}}
+}
