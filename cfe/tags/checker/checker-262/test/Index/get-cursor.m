d71af27b683e08606bc7de9d3f7e8872618186eb
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2012-03-15 22:41:49 +0000

    Tagging checker-262.

---
+++ cfe/tags/checker/checker-262/test/Index/get-cursor.m
@@ -0,0 +1,52 @@
+// Test is line- and column-sensitive. Run lines are below.
+
+@interface rdar9771715
+@property (readonly) int foo1;
+@property (readwrite) int foo2;
+@end
+
+@class Foo;
+
+@interface rdar9535717 {
+  __weak Foo *foo;
+}
+@end
+
+@interface Test1 {
+  id _name;
+}
+@end
+@interface Test1 ()
+- (id)name;
+@end
+@interface Test1 ()
+@property (copy) id name;
+@end
+@implementation Test1
+@synthesize name = _name;
+@end
+
+@interface rdar10902015
+@end
+
+@implementation rdar10902015
+
+struct S {};
+
+-(void)mm:(struct S*)s {
+  rdar10902015 *i = 0;
+}
+@end
+
+// RUN: c-index-test -cursor-at=%s:4:28 -cursor-at=%s:5:28 %s | FileCheck -check-prefix=CHECK-PROP %s
+// CHECK-PROP: ObjCPropertyDecl=foo1:4:26
+// CHECK-PROP: ObjCPropertyDecl=foo2:5:27
+
+// RUN: c-index-test -cursor-at=%s:11:11 %s -target x86_64-apple-macosx10.7.0 | FileCheck -check-prefix=CHECK-WITH-WEAK %s
+// CHECK-WITH-WEAK: ObjCClassRef=Foo:8:8
+
+// RUN: c-index-test -cursor-at=%s:20:10 %s | FileCheck -check-prefix=CHECK-METHOD %s
+// CHECK-METHOD: ObjCInstanceMethodDecl=name:20:1
+
+// RUN: c-index-test -cursor-at=%s:37:17 %s | FileCheck -check-prefix=CHECK-IN-IMPL %s
+// CHECK-IN-IMPL: VarDecl=i:37:17
