ae2ca2398da3626d86a6058880339baba9dae697
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2012-04-26 06:53:44 +0000

    Tagging checker-264.

---
+++ cfe/tags/checker/checker-264/test/Sema/attr-visibility.c
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify %s
+
+void test0() __attribute__((visibility("default")));
+void test1() __attribute__((visibility("hidden")));
+void test2() __attribute__((visibility("internal")));
+
+// rdar://problem/10753392
+void test3() __attribute__((visibility("protected"))); // expected-warning {{target does not support 'protected' visibility; using 'default'}}
+
+struct __attribute__((visibility("hidden"))) test4; // expected-note {{previous attribute is here}}
+struct __attribute__((visibility("default"))) test4; // expected-error {{visibility does not match previous declaration}}
