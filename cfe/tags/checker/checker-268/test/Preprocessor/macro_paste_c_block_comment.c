8eeec7345ecf1e6be63e755cdf7b98007c3425cb
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2012-07-02 23:23:36 +0000

    Removing checker-268.

--- cfe/tags/checker/checker-268/test/Preprocessor/macro_paste_c_block_comment.c
+++
@@ -1,8 +0,0 @@
-// RUN: %clang_cc1 %s -Eonly -verify
-
-#define COMM / ## *
-COMM // expected-error {{pasting formed '/*', an invalid preprocessing token}}
-
-// Demonstrate that an invalid preprocessing token
-// doesn't swallow the rest of the file...
-#error EOF // expected-error {{EOF}}
